ccc3707e465381ecb37d0f038f9370d2dc8dda32
+ <+>
2021-10-14 13:13:46 -0600

    commit tree 1eb90b6e2341d51010bc77b96010f61c06d288e2

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 
+
+function superbowlWin(array) {
+    for (let i = 0; i < array.length; i++) {
+    if(array[i].result === "W") {
+        return array[i].year;
+    }
+} 
+}
+
+// const suberbowlWin = array.find(element => array.element === "W") {
+//     return array.element[0];
+// }
+// function isWin (element, index, array) {
+//    for (const element of array) {
+//        if (array.element === "W") {
+//            return array[index];
+//        }
+//    }
+// }
+// function superbowlWin(array) {
+//     return array.find();
+// }   
+
+// console.log(retur)
+
+// // const record = [
+//     {year: "2018", result: "L"},
+//     {year: "2017", result: "W"},
+//     {year: "2016", result: "N/A"}
+//     //...
+//   ]
+
